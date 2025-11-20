328ceb0e3cbb592466413fb78e030a01f6359de9
diasmendelsson <mendersson@hotmail.com>
2025-11-20 15:08:48 -0300

    Initial commit from Create Next App

---
+++ dados/dados.js
@@ -0,0 +1,7 @@
+
+const produtos = [
+    { id: 1, nome: "Alcatra", preco: 12.0, imagem: "/alcatra.jpg", categoria: "Espetos" },
+    { id: 2, nome: "Frango com Bacon", preco: 10.0, imagem: "/frango-bacon.jpg", categoria: "Espetos" },
+    { id: 3, nome: "Kafta", preco: 13.0, imagem: "/kafta.jpg", categoria: "Espetos" },
+    { id: 4, nome: "Espeto de Queijo Coalho", preco: 9.0, imagem: "/queijo.jpg", categoria: "Espetos" },
+]
